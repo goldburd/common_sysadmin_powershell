8170b1f6634f9b36def4d39ed619e8b85bd6f41e
goldburd <goldburd@buffalo.edu>
2020-03-08 20:42:18 -0400

    Add files via upload
    bulk upn suffix change on prem AD

---
+++ changeusersupnsuffix.ps1
@@ -0,0 +1,3 @@
+﻿$upnSuffix = Read-Host -Prompt 'Enter New UPN Suffix'
+$LocalUsers = Get-ADUser -Filter {UserPrincipalName -like (Get-ADForest).Name} -Properties userPrincipalName -ResultSetSize $null
+$LocalUsers | foreach {$newUpn = $_.UserPrincipalName.Replace((Get-ADForest).Name,$upnSuffix); $_ | Set-ADUser -UserPrincipalName $newUpn}
